9bd6f9833a11c488f918bfc835e81927706d6143
Feranmi <femisolomon97@gmail.com>
2026-03-21 12:36:29 -0700

    Update layout spacing and borders

---
+++ fen.js
@@ -0,0 +1,18 @@
+let age = 50;
+
+if (age < 13){
+    console.log("you are a child")
+
+}else if (age >= 13 && age <= 19){
+    console.log("you are a teenager")
+
+}else {
+    console.log("you are an adult")
+}
+
+
+function button1 (name){
+    console.log("welcome" + name)
+
+}
+button1(" femi")
